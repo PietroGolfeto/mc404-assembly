ca286f3a3484a1d92ae15ff71d0a1b6b969f475b
PietroGolfeto <pggolfeto@gmail.com>
2023-05-27 19:58:10 -0300

    Fixes sign extension problem
    Works with the following assembly risc-V instructions: 
    addi, slli, xor, call, ret, jalr, beq, lw, sw, mul, lui, auipc, li

--- at2.py
+++ at2.py
@@ -34,13 +34,13 @@ def instrucao_I(lista):
             func3 = "010"
             opcode = "0000011"
         case "ret":
-            # Implementação de jalr ( jalr zero, 0(ra) )
+            # Implementação de jalr zero, ra, 0
             return instrucao_I(["jalr", "zero", "ra", "0"])
         case "jalr":
             func3 = "000"
             opcode = "1100111"
 
-    # Imediato é o terceiro elemento da lista exceto se for lw    
+    # Imediato é o terceiro elemento da lista exceto se for lw
     num_decimal = int(lista[3]) if lista[3].isdigit() else int(lista[2])
     # Converte em hexadecimal e adiciona prefixo 0x
     num_hex = "0x" + format(num_decimal, "03x").upper()
@@ -52,12 +52,12 @@ def instrucao_I(lista):
     rs1 = format(registradores[rs1_string], "05b")
 
     final_string = rs1 + func3 + rd + opcode
-    final_hex = hex(int(final_string, 2))
 
     # Completa com zeros à esquerda para ter 8 caracteres (3 do num_hex + 5 do final_hex)
     # Coloca em maiusculo, exceto o prefixo 0x
-    final = final_hex[2:].zfill(5).upper()
-    return num_hex + final
+    final_hex = hex(int(final_string, 2))[2:].zfill(5).upper()
+    final = num_hex + final_hex
+    return final
 
 
 def instrucao_R(lista):
@@ -82,10 +82,10 @@ def instrucao_R(lista):
     # Formatação da instrução do tipo R em binário em Assembly Risc-V
     final_string = funct7 + rs2 + rs1 + func3 + rd + opcode
     final_hex = hex(int(final_string, 2))
+
     # Completa com zeros à esquerda para ter 8 caracteres
     # Coloca em maiusculo, exceto o prefixo 0x
     final = final_hex[:2] + final_hex[2:].zfill(8).upper()
-
     return final
 
 
@@ -98,7 +98,8 @@ def instrucao_S(lista):
     num_decimal = int(lista[2])
     # Converte em binário utilizando complemento de dois com 12 bits
     tamanho = 12
-    num_bin = format(num_decimal & ((1 << tamanho) - 1), f"0{tamanho}b").zfill(tamanho)
+    num_bin = format(num_decimal & ((1 << tamanho) - 1),
+                     f"0{tamanho}b").zfill(tamanho)
 
     # Divide o número binário em dois imediatos, um de 7 bits e outro de 5 bits
     imm7 = num_bin[:7]
@@ -125,7 +126,7 @@ def instrucao_SB(lista):
         case "beq":
             func3 = "000"
             opcode = "1100011"
-    
+
     rs1_string = lista[1]
     rs1 = format(registradores[rs1_string], "05b")
 
@@ -138,7 +139,8 @@ def instrucao_SB(lista):
 
     # Converte em binário utilizando complemento de dois com 13 bits
     tamanho = 13
-    num_bin = format(offset & ((1 << tamanho) - 1), f"0{tamanho}b").zfill(tamanho)
+    num_bin = format(offset & ((1 << tamanho) - 1),
+                     f"0{tamanho}b").zfill(tamanho)
 
     # Divide o número binário em quatro imediatos, dois de 1 bit, um de 6 bits e um de 4 bits
     # Ignora o bit menos significativo (não utilizado em instruções do tipo SB)
@@ -167,7 +169,8 @@ def instrucao_U(lista):
     num_decimal = int(lista[2])
     # Converte em binário utilizando complemento de dois com 20 bits
     tamanho = 20
-    imm20 = format(num_decimal & ((1 << tamanho) - 1), f"0{tamanho}b").zfill(tamanho)
+    imm20 = format(num_decimal & ((1 << tamanho) - 1),
+                   f"0{tamanho}b").zfill(tamanho)
 
     rd_string = lista[1]
     rd = format(registradores[rd_string], "05b")
@@ -194,12 +197,13 @@ def instrucao_J(lista):
     offset = num_decimal - pos_inicial
     # Converte em binário utilizando complemento de dois com 32 bits
     tamanho = 32
-    num_bin = format(offset & ((1 << tamanho) - 1), f"0{tamanho}b").zfill(tamanho)
+    num_bin = format(offset & ((1 << tamanho) - 1),
+                     f"0{tamanho}b").zfill(tamanho)
 
     # Divide o número binário em dois imediatos, um de 20 bits e outro de 12 bits
     imm20 = num_bin[:20]
     # Transforma imm20 em string representando inteiro de base dez no argumento da instrução auipc
-    # Primeira instrução de call é auipc 
+    # Primeira instrução de call é auipc
     # Simulador Venus utliza o registrador t1 para armazenar o endereço de salto
     auipc = mapa["auipc"](["auipc", "t1", str(int(imm20, 2))])
 
@@ -210,14 +214,14 @@ def instrucao_J(lista):
     # Registrador ra é utilizado para armazenar o endereço de retorno
     jalr = mapa["jalr"](["jalr", "ra", "t1", str(int(imm12, 2))])
 
-    return auipc + "\n" + jalr
+    return auipc, jalr
 
 
 def instrucao_dupla(lista):
     match lista[0]:
         case "li":
             pass
-    
+
     # Instrução li pode ser composta por duas instruções, dependendo do tamanho do imediato
     # Se o imediato for maior que 12 signed bits, é necessário também utilizar a instrução lui
     # Caso contrário, apenas a instrução addi é necessária
@@ -225,18 +229,24 @@ def instrucao_dupla(lista):
     if (num_decimal > ((1 << 11) - 1) or num_decimal < -2048):
         # Converte em binário utilizando complemento de dois com 32 bits
         tamanho = 32
-        num_bin = format(num_decimal & ((1 << tamanho) - 1), f"0{tamanho}b").zfill(tamanho)
+        num_bin = format(num_decimal & ((1 << tamanho) - 1),
+                         f"0{tamanho}b").zfill(tamanho)
 
         # Divide o número binário em dois imediatos, um de 20 bits e outro de 12 bits
         imm20 = num_bin[:20]
-        # Transforma imm20 em string representando inteiro de base dez no argumento da instrução lui
-        lui = mapa["lui"](["lui", "t1", str(int(imm20, 2))])
-
         imm12 = num_bin[20:]
-        # Transforma imm12 em string representando inteiro de base dez no argumento da instrução addi
-        addi = mapa["addi"](["addi", lista[1], "t1", str(int(imm12, 2))])
 
-        return lui + "\n" + addi
+        # Se o imediato de 12 bits for negativo, soma 1 ao imediato de 20 bits para extensão de sinal
+        if (imm12[0] == "1"):
+            imm20 = format(int(imm20, 2) + 1, "020b")
+
+        # Transforma imm20 em string representando inteiro de base dez no argumento da instrução lui
+        lui = mapa["lui"](["lui", lista[1], str(int(imm20, 2))])
+
+        # Transforma imm12 em string representando inteiro de base dez no argumento da instrução addi
+        addi = mapa["addi"](["addi", lista[1], lista[1], str(int(imm12, 2))])
+
+        return lui, addi
     else:
         # Transforma num_decimal em string representando inteiro de base dez no argumento da instrução addi
         addi = mapa["addi"](["addi", lista[1], "zero", str(num_decimal)])
@@ -246,34 +256,42 @@ def instrucao_dupla(lista):
 
 # Dicionario que mapeia as instrucoes para as funcoes
 mapa = {
-    "addi": instrucao_I, #OK
-    "slli": instrucao_I, #OK
-    "xor": instrucao_R, #OK
-    "call": instrucao_J, #OK
-    "ret": instrucao_I, #jalr #OK
-    "jalr": instrucao_I, #OK
-    "beq": instrucao_SB, #OK
-    "lw": instrucao_I, #OK
-    "sw": instrucao_S, #OK
-    "mul": instrucao_R, #OK
-    "lui": instrucao_U, #OK
-    "auipc": instrucao_U, #OK
-    "li": instrucao_dupla # FALTA EXTENSÃO DE SINAL QUANDO QUEBRAR EM 2 INSTRUÇÕES
-    # SE BIT 12 FOR 1, EXTENSÃO DE SINAL
+    "addi": instrucao_I,
+    "slli": instrucao_I,
+    "xor": instrucao_R,
+    "call": instrucao_J,
+    "ret": instrucao_I,
+    "jalr": instrucao_I,
+    "beq": instrucao_SB,
+    "lw": instrucao_I,
+    "sw": instrucao_S,
+    "mul": instrucao_R,
+    "lui": instrucao_U,
+    "auipc": instrucao_U,
+    "li": instrucao_dupla
 }
 
-
+# Loop para receber as instrucoes e chamar a funcao correspondente até que o usuario digite uma linha vazia
+print("Instruções disponíveis: addi, slli, xor, call, ret, jalr, beq, lw, sw, mul, lui, auipc, li")
 instrucao = input("Digite a instrução desejada: ")
 while (instrucao != ""):
-    lista = instrucao.replace(",", "").replace("(", " ").replace(")", "").split()
-    print(lista)
+    # Elimina vírgulas e parênteses e separa a string em uma lista
+    lista = instrucao.replace(",", "").replace(
+        "(", " ").replace(")", "").split()
 
     if lista[0] in mapa:
         funcoes = mapa[lista[0]]
         codigo = funcoes(lista)
-        print(codigo)
+
+        if isinstance(codigo, str):
+            print(codigo)
+        elif isinstance(codigo, tuple):
+            for value in codigo:
+                print(value)
 
     else:
         "Instrução inválida"
 
+    print()
+    print("Instruções disponíveis: addi, slli, xor, call, ret, jalr, beq, lw, sw, mul, lui, auipc, li")
     instrucao = input("Digite a instrução desejada: ")
